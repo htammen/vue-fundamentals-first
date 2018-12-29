ded50b801dcba22e6f3449116998873cec9aab64
Helmut Tammen <h.tammen@tammen-it-solutions.de>
2018-12-29 12:40:33 +0000

    Added a vue-fundamentals table

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 ## Running app on gitpod
 - `npm run dev`
 - open a new terminal
-  - gp forward-port 9000 9001
-  
+  - `gp forward-port 9000 9001`
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue';
 import AppHelmut from './src/index.vue'
 
+import FundamentalVue from 'fundamental-vue';
+Vue.use(FundamentalVue);
+
 new Vue({
     el: "#demo",
     components: {
-        app: AppHelmut 
+        app: AppHelmut
     }
 });
